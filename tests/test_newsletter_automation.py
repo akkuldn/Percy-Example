14db02c68d527b91768729b569f097231ab99a10
akkuldn <akkul.dn@qxf2.com>
2023-12-22 12:27:21 +0530

    Added simple selenium test

---
+++ tests/test_newsletter_automation.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from percy import percy_snapshot
+
+driver = webdriver.Chrome()
+
+
+def test_title():
+	driver.get("https://staging-newsletter-generator.qxf2.com")
+
+	title = driver.title
+
+	assert title == "Home"
+
+	percy_snapshot(driver=driver,name="Home page")
+
+	driver.quit()
